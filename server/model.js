2b458ba5c0d5dd5ca2bb8c1a49ddc4874576d461
Jake <jakedavisemail@gmail.com>
2023-01-25 12:40:08 -0700

    Created functions in model.js

--- server/model.js
+++ server/model.js
@@ -1,6 +1,31 @@
 //Integrate API Access
 const axios = require('axios')
 
-module.exports = {
+const headers = {
+  'Authorization': process.env.AUTH;
+}
 
+module.exports = {
+  get: url => {
+    return axios({
+      method: 'GET',
+      url: url,
+      headers: headers
+    })
+  },
+  post: (url, data) => {
+    return axios({
+      method: 'POST',
+      url: url,
+      data: data,
+      headers: headers
+    })
+  },
+  put: (url, data) => {
+    return axios({
+      method: 'PUT',
+      url: url,
+      headers: headers
+    })
+  }
 }
