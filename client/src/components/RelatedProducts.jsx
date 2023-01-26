3e1b48261ae9366da1ba83a670f4b66d8601f9d5
alibacova <libacova.a@gmail.com>
2023-01-25 17:10:22 -0800

    Created RelatedProducts components

---
+++ client/src/components/RelatedProducts.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+const RelatedProducts = () => {
+
+  return (
+    <h5>RELATED PRODUCTS</h5>
+    <section>
+      Cards will go here
+    </section>
+    <h5>YOUR OUTFIT</h5>
+    <section>
+      Outfit cards will go here
+    </section>
+  );
+}
+
+export default RelatedProducts;
