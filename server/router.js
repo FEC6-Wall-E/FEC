b7902a0e6f842b426c9acd1f50947a1fdad619fa
Jake <jakedavisemail@gmail.com>
2023-01-25 17:33:24 -0700

    Working getAnswers and getQuestions functions

--- server/router.js
+++ server/router.js
@@ -12,22 +12,22 @@ router.get('/products/:pid/related', controller.products.getRelated)
 router.get('/reviews/:pid', controller.reviews.getReviews)
 router.get('/meta/:pid', controller.reviews.getMeta)
 
-router.post('/reviews/:pid', controller.reviews.post)
+router.post('/reviews', controller.reviews.post)
 
 router.put('/reviews/:rid/helpful', controller.reviews.helpful)
 router.put('/reviews/:rid/report', controller.reviews.report)
 
 
 // Q&A ROUTES
-router.get('/products/:pid/questions', controller.questions.getQuestions)
-router.get('/products/:pid/answers', controller.questions.getAnswers)
+router.get('/qa/:pid/questions', controller.questions.getQuestions)
+router.get('/qa/:qid/answers', controller.questions.getAnswers)
 
-router.post('/products/:pid/questions', controller.questions.postQuestion)
-router.post('/products/:pid/questions/:qid/answers', controller.questions.postAnswer)
+router.post('/qa/:pid/questions', controller.questions.postQuestion)
+router.post('/qa/:pid/questions/:qid/answers', controller.questions.postAnswer)
 
-router.put('/products/:pid/questions/:qid/helpful', controller.questions.questionHelpful)
-router.put('/products/:pid/questions/:qid/report', controller.questions.questionReport)
-router.put('/products/:pid/questions/:qid/answers/:aid/helpful', controller.questions.answerHelpful)
-router.put('/products/:pid/questions/:qid/answers/:aid/report', controller.questions.answerReport)
+router.put('/qa/:pid/questions/:qid/helpful', controller.questions.questionHelpful)
+router.put('/qa/:pid/questions/:qid/report', controller.questions.questionReport)
+router.put('/qa/:pid/questions/:qid/answers/:aid/helpful', controller.questions.answerHelpful)
+router.put('/qa/:pid/questions/:qid/answers/:aid/report', controller.questions.answerReport)
 
 module.exports = router;
