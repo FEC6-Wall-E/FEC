b362e42477b535379d9d1771fe9bb81aa75ecc19
Jake <jakedavisemail@gmail.com>
2023-01-26 12:11:32 -0700

    Added state for styles and product Added example / init data

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Overview from './overview/index.jsx';
+import examples from '../examples.js';
 
-function App(props) {
-  const test = { name: 'test ' };
+function App() {
+  const [product, setProduct] = React.useState(examples.product);
+  const [style, setStyle] = React.useState(examples.styles.results[0]);
+
   return (
     <div>
-      <Overview product={test} />
-      Hello World
+      <Overview product={product} style={style} setStyle={setStyle} />
     </div>
   );
 }
