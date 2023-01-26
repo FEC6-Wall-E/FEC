5110f2c7fe795a66e244a7e280edcf9159c29beb
alibacova <libacova.a@gmail.com>
2023-01-26 07:44:18 -0800

    Set up initial Related Products structure

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,11 +1,20 @@
 import React, {useState} from "react";
 
-const RelatedProducts = () => {
+const RelatedProducts = ({product}) => {
+  // const [relatedList, setRelatedList] = useState([]);
+
+  // const getRelatedProducts = () => {
+  //   axios.get(`/products/${product.id}/related`)
+  //     .then((products) => {
+  //       setRelatedList(products);
+  //     })
+  //     .catch((err) => console.error(err));
+  // }
 
   return (
     <h5>RELATED PRODUCTS</h5>
     <section>
-      Cards will go here
+      Related product cards will go here
     </section>
     <h5>YOUR OUTFIT</h5>
     <section>
