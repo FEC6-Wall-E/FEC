7f5d87090c77c4d51def301b54c8ced339ead457
alibacova <libacova.a@gmail.com>
2023-01-27 12:37:57 -0800

    Add conditional rendering for price in Related products

--- client/src/components/relatedItemsAndComparison/ProductCard.jsx
+++ client/src/components/relatedItemsAndComparison/ProductCard.jsx
@@ -1,24 +1,25 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ProductCard = ({productId}) => {
+function ProductCard({ productId }) {
   const initialDefault = {
-    'name': '',
+    name: '',
     'default?': true,
-    'original_price': '',
-    'photos': [{
-        'thumbnail_url': 'https://www.freeiconspng.com/uploads/no-image-icon-6.png',
-        'url': ''
-      }],
-    'sale_price': null,
-    'skus': {},
-    'style_id': '',
+    original_price: '',
+    photos: [{
+      thumbnail_url: 'https://www.freeiconspng.com/uploads/no-image-icon-6.png',
+      url: '',
+    }],
+    sale_price: null,
+    skus: {},
+    style_id: '',
   };
 
   const [relatedProduct, setRelatedProduct] = useState({});
   const [defaultStyle, setDefaultStyle] = useState(initialDefault);
+  const [rating, setRating] = useState(0);
 
-  //get info for the current related product (needed for )
+  // get info for the current related product (needed for )
   const getRelatedProduct = () => {
     if (productId) { //had to add the condition for initial render when no
                      //productId was provided; adjust when change useEffect
@@ -26,11 +27,12 @@ const ProductCard = ({productId}) => {
       axios.get(`/products/${productId}`)
         .then((product) => {
           setRelatedProduct(product.data);
+          getRating();
         })
         .catch((err) => console.error(err))
         .then(() => getDefaultStyles());
     }
-  }
+  };
 
   //get default style for the current related product (needed for images and price)
   const getDefaultStyles = () => {
@@ -39,17 +41,35 @@ const ProductCard = ({productId}) => {
                       //dependencies
       axios.get(`/products/${productId}/styles`)
       .then((product) => {
-        let defaultStyles = product.data.results.filter((defaultStyle) => defaultStyle['default?']);
-        if (defaultStyles.length > 0) {
-          setDefaultStyle(defaultStyles[0]);
-        } else {
-          setDefaultStyle(product.data.results[0]);
-        }
+        const defaultStyles = product.data.results.filter((defaultStyle) => defaultStyle['default?']);
+          if (defaultStyles.length > 0) {
+            setDefaultStyle(defaultStyles[0]);
+          } else {
+            setDefaultStyle(product.data.results[0]);
+          }
       })
       .catch((err) => console.error(err));
     }
-  }
+  };
 
+  const getRating = () => {
+    if (productId) { //had to add the condition for initial render when no
+                     //productId was provided; adjust when change useEffect
+                     //dependencies
+      axios.get(`/meta/${productId}`)
+        .then((meta) => {
+          setRating(calculateRating(meta.data.ratings));
+        })
+        .catch((err) => console.error(err))
+    }
+  };
+
+  const calculateRating = (data) => {
+    var ratings = Object.values(data);
+    var dividend = ratings.reduce((accumulator, currentValue, currentIndex) => accumulator + +currentValue * (currentIndex + 1), 0);
+    var divisor = ratings.reduce((accumulator, currentValue) => accumulator + +currentValue, 0);
+    return Math.round(dividend / divisor * 10) / 10;
+  }
 
   useEffect(() => {
     getRelatedProduct();
@@ -61,15 +81,24 @@ const ProductCard = ({productId}) => {
       {
         !defaultStyle.photos[0].thumbnail_url
         ?
-        <img src='https://www.freeiconspng.com/uploads/no-image-icon-6.png'/>
+          <img src='https://www.freeiconspng.com/uploads/no-image-icon-6.png'/>
         :
         <img src={`${defaultStyle.photos[0].thumbnail_url}`}/>
-      }
+       }
       <h4>{relatedProduct.name}</h4>
       <div className="category">{relatedProduct.category}</div>
-      <div className="defaultPrice">{relatedProduct.default_price}</div>
-      {/*
-      {starRating} */}
+      {
+        defaultStyle.sale_price
+        ?
+        <div className="price">
+          <span className="sale"> {`$${defaultStyle.sale_price} `} </span>
+          <span className="struckthrough"> {`$${defaultStyle.original_price}`} </span>
+          </div> //do we want to have decimal point and decimals?
+        :
+        <div className="price default">{`$${defaultStyle.original_price}`}</div> //do we want to have decimal point and decimals?
+      }
+
+      Rating:{rating}
     </div>
   );
 }
