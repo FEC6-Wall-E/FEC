22832609f9b104c18ddebc05d05435056d47e425
Jake <jakedavisemail@gmail.com>
2023-01-30 18:13:46 -0700

    Added FontAwesome kit and created working twitter button

--- client/src/components/overview/info/index.jsx
+++ client/src/components/overview/info/index.jsx
@@ -1,17 +1,28 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TwitterShareButton } from 'react-twitter-embed';
 import getRatingData from '../../sharedComponents/lib/averageRating.js';
 import StarRating from '../../sharedComponents/StarRating.jsx';
 import Price from './price.jsx';
 
 function ProductInfo({ metaData, product, style }) {
   const ratingData = getRatingData(metaData);
+  const twitterText = `Checkout this ${product.name} from Atelier! Get it at http://localhost:3000/${product.id}`;
 
   return (
     <div id="ProductInfo">
       <StarRating count={ratingData.ratings} rating={ratingData.averageRating} />
-      <h2>{product.category}</h2>
-      <h1>{product.name}</h1>
+      <h2 className="infoCategory">{product.category}</h2>
+      <h1 className="infoName">{product.name}</h1>
       <Price original={style.original_price} sale={style.sale_price} />
+      <p className="infoDescription">{product.description}</p>
+      <div id="shareButtons">
+        <i
+          onClick={() => window.open(`https://twitter.com/intent/tweet?text=${twitterText}`)}
+          className="fa-brands fa-twitter fa-2xl shareButton"
+        />
+      </div>
     </div>
   );
 }
