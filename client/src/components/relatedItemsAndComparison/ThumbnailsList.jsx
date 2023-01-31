aadaed0c13cc906f0218c077d9f83b19b2135bfb
alibacova <libacova.a@gmail.com>
2023-01-31 15:06:02 -0800

    Clean up code

--- client/src/components/relatedItemsAndComparison/ThumbnailsList.jsx
+++ client/src/components/relatedItemsAndComparison/ThumbnailsList.jsx
@@ -3,29 +3,14 @@ import { flushSync } from 'react-dom';
 import Thumbnail from './Thumbnail.jsx';
 
 function ThumbnailsList ({ images, id, setMainImg }) {
-  // console.log('thumbnails ', images);
   const [index, setIndex] = useState(0);
   const [length, setLength] = useState(images.length);
   const ref = useRef(null);
   const imgsToDisplay = 4;
 
-  // console.log('showThumbnails ', showThumbnails);
-
   useEffect(() => {
     setLength(images.length);
   }, [images] );
-
-  // const handlePrevious = () => {
-  //   if (currIdx > 0) {
-  //     setIndex(index - 1);
-  //   }
-  // };
-
-  // const handleNext = () => {
-  //   if (currIdx < length - imgsToDisplay) {
-  //     setIndex(index + 1);
-  //   }
-  // }
 
   const handleNav = (direction) => {
     flushSync(() => {
@@ -71,23 +56,10 @@ function ThumbnailsList ({ images, id, setMainImg }) {
             setMainImg={setMainImg}
           />
         )}
-        {/* <div className="carousel container"
-        style={{ transform: `translateX(-${index * (100 / imgsToDisplay)}%)`}}>
-          {images.map((thumbnail, idx) => <Thumbnail key={idx} thumbnail={thumbnail} />)}
-        </div> */}
-        {/* {images.map((thumbnail, index) => {
-          // console.log('here ', thumbnail);
-          return (
-            <div key={index}>
-              {index >= 0 && index <= 4 && (
-                <Thumbnail thumbnail={thumbnail} setMainImg={setMainImg}/>
-              )}
-              </div>
-          );
-        })} */}
       </div>
     </div>
   );
 }
 
 export default ThumbnailsList;
+{/* style={{ transform: `translateX(-${index * (100 / imgsToDisplay)}%)`}}> */}
