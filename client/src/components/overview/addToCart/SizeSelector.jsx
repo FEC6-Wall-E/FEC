cd9192ac9a4224c0725932f4d4187caffd16c92e
Jake <jakedavisemail@gmail.com>
2023-01-31 17:18:24 -0700

    MVP Size Selector

--- client/src/components/overview/addToCart/SizeSelector.jsx
+++ client/src/components/overview/addToCart/SizeSelector.jsx
@@ -8,12 +8,16 @@ function SizeSelector({ currentStyle, setSize }) {
     }
   });
 
-  console.log(sizes);
-
-  return (
+  return sizes.length === 0 ? (
     <span id="SizeSelector">
-      <select name="size" default="Select Size" onChange={(e) => setSize(e.target.value)}>
-        <option key="-1" value="Select Size" selected>Select Size</option>
+      <select name="size" disabled>
+        <option key="-1" value="OOS" selected>OUT OF STOCK</option>
+      </select>
+    </span>
+  ) : (
+    <span id="SizeSelector">
+      <select value="Select Size" name="size" onChange={(e) => setSize(e.target.value)}>
+        <option key="-1" value="Select Size">Select Size</option>
         {sizes.map((size, idx) => <option key={idx} value={size}>{size}</option>)}
       </select>
     </span>
