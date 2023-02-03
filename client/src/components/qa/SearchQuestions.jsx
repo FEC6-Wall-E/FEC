a0c98e92ec3c20a31d1c84add058d10cdc8a18ef
Daniel Greaves <dgreaves18@gmail.com>
2023-02-02 22:42:55 -0800

    Added alert when no questons match input, resets form to empty string

--- client/src/components/qa/SearchQuestions.jsx
+++ client/src/components/qa/SearchQuestions.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function SearchQuestions({ setSearchInput }) {
   return (
     <form id="SearchQuestions" onSubmit={(e) => { e.preventDefault(); }}>
-      <input id="SearchBar" type="text" onChange={(e) => { setSearchInput(e.target.value); }} placeholder="Have a question? Search for answers..." />
+      <input
+        id="SearchBar"
+        type="text"
+        onChange={(e) => { setSearchInput(e.target.value); }}
+        onFocus={(e) => { e.target.value = ''; }}
+        placeholder="Have a question? Search for answers..."
+      />
     </form>
   );
 }
