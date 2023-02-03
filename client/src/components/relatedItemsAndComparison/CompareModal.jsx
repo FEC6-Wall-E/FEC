b59b53c1f52083c03029620db461974185bf998d
alibacova <libacova.a@gmail.com>
2023-02-02 20:48:12 -0800

    Set up CompareModal component draft

--- client/src/components/relatedItemsAndComparison/CompareModal.jsx
+++ client/src/components/relatedItemsAndComparison/CompareModal.jsx
@@ -1,9 +1,26 @@
-// import React, {useState, useEffect} from 'react';
-// import axios from 'axios';
-// import ProductCard from './ProductCard.jsx';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import getAllFeatures from './getAllFeatures.js';
+import examples from '../.././examples.js';
 
-function Comparison() {
+function CompareModal({ product2, setShowModal }) {
+  const features = getAllFeatures(examples.product.features, product2.features);
 
+  return (
+    <div id="comparison-modal">
+      <table>
+        <tr>
+          <th>Product 1 name</th>
+          <th></th>
+          <th>Product 2 name</th>
+        </tr>
+        <tr>Feature value for product 1</tr>
+        <tr>Feature name</tr>
+        <tr>Feature value for product 2</tr>
+      </table>
+      <button onClick={() => setShowModal(false)}>Close</button>
+    </div>
+  )
 }
 
-export default Comparison;
+export default CompareModal;
--- client/src/components/relatedItemsAndComparison/ProductCard.jsx
+++ client/src/components/relatedItemsAndComparison/ProductCard.jsx
@@ -4,9 +4,10 @@ import { API } from '../.././api.js';
 import ThumbnailsList from './ThumbnailsList.jsx';
 import averageRating from '../sharedComponents/lib/averageRating.js';
 import StarRating from '../sharedComponents/StarRating.jsx';
+import CompareModal from './CompareModal.jsx'
 
 /* eslint prefer-arrow-callback: [ "error", { "allowNamedFunctions": true } ] */
-const ProductCard = forwardRef(function ProductCard({ productId, index, idx }, ref) {
+const ProductCard = forwardRef(function ProductCard({ relatedProductId, index, idx }, ref) {
   const [relatedProduct, setRelatedProduct] = useState({});
   const [defaultStyle, setDefaultStyle] = useState({});
   const [rating, setRating] = useState({});
@@ -15,10 +16,11 @@ const ProductCard = forwardRef(function ProductCard({ productId, index, idx }, r
     url: 'https://www.freeiconspng.com/uploads/no-image-icon-6.png',
   }]);
   const [mainImg, setMainImg] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const getRating = () => {
-    if (productId) {
-      axios.get(`/meta/${productId}`)
+    if (relatedProductId) {
+      axios.get(`/meta/${relatedProductId}`)
         .then((meta) => {
           setRating(averageRating(meta.data));
         })
@@ -28,7 +30,7 @@ const ProductCard = forwardRef(function ProductCard({ productId, index, idx }, r
 
   // get default style for the current related product (needed for images and price)
   const getStyles = () => {
-    axios.get(`/products/${productId}/styles`)
+    axios.get(`/products/${relatedProductId}/styles`)
       .then((product) => {
         const defaultStyles = product.data.results.filter((style) => style['default?']);
         const newDefaultStyle = defaultStyles.length > 0
@@ -44,7 +46,7 @@ const ProductCard = forwardRef(function ProductCard({ productId, index, idx }, r
 
   // get info for the current related product (needed for )
   const getRelatedProductData = () => {
-    axios.get(`/products/${productId}`)
+    axios.get(`/products/${relatedProductId}`)
       .then((product) => {
         setRelatedProduct(product.data);
         getRating();
@@ -55,21 +57,16 @@ const ProductCard = forwardRef(function ProductCard({ productId, index, idx }, r
   };
 
   useEffect(() => {
-    if (productId) {
+    if (relatedProductId) {
       getRelatedProductData();
     }
-  }, [productId]);
+  }, [relatedProductId]);
 
   useEffect(() => {
     if (images.length > 0) {
       setMainImg(images[0].thumbnail_url);
     }
   }, [images]);
-
-
-  // const handleNavigate = () => {
-  //   setProduct(productId);
-  // };
 
   return (
     // add onClick={handleNavigate} to the first div
@@ -80,7 +77,8 @@ const ProductCard = forwardRef(function ProductCard({ productId, index, idx }, r
       <img className="mainImg" src={mainImg} alt="Missing" />
       <ThumbnailsList images={images} setMainImg={setMainImg} />
       {/* add onClick={handleCompare} to the button */}
-      <button className="compare">Compare</button>
+      <button className="compare" onClick={() => setShowModal(true)}>Compare</button>
+      {showModal && <CompareModal product2={relatedProduct} setShowModal={setShowModal} />}
       <div className="category">{relatedProduct.category}</div>
       <h4 className="product-name">{relatedProduct.name}</h4>
       {defaultStyle.sale_price
