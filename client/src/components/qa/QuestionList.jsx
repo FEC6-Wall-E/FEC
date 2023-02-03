58d9d89525720ea88c8e69ac7b44aee49bc769b3
Daniel Greaves <dgreaves18@gmail.com>
2023-02-02 22:40:33 -0800

    Moved AddQuestion component from QandA into List for formatting

--- client/src/components/qa/QuestionList.jsx
+++ client/src/components/qa/QuestionList.jsx
@@ -2,8 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Question from './Question.jsx';
+import AddQuestion from './AddQuestion.jsx';
 
-function QuestionList({ questionList }) {
+function QuestionList({ questionList, getQuestions }) {
   const [questionCount, setQuestionCount] = useState(2);
 
   if (questionCount >= questionList.length) {
@@ -13,8 +14,10 @@ function QuestionList({ questionList }) {
           <Question
             body={question.question_body}
             helpfulness={question.question_helpfulness}
+            id={question.question_id}
             key={question.question_id}
             answers={question.answers}
+            getQuestions={getQuestions}
           />
         ))}
       </div>
@@ -26,13 +29,16 @@ function QuestionList({ questionList }) {
         <Question
           body={question.question_body}
           helpfulness={question.question_helpfulness}
+          id={question.question_id}
           key={question.question_id}
           answers={question.answers}
+          getQuestions={getQuestions}
         />
       ))}
       <button onClick={(e) => { setQuestionCount(questionCount + 2); }}>
         More Answered Questions
       </button>
+      <AddQuestion />
     </div>
   );
 }
