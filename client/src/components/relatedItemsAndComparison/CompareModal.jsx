fd7f018ccb23d70444050e9b852c79e3b4080691
alibacova <libacova.a@gmail.com>
2023-02-06 11:14:10 -0800

    Add overall layout styling to comparison modal

--- client/src/components/relatedItemsAndComparison/CompareModal.jsx
+++ client/src/components/relatedItemsAndComparison/CompareModal.jsx
@@ -7,23 +7,33 @@ function CompareModal({ product2, setShowModal }) {
   const features = getAllFeatures(examples.product.features, product2.features);
 
   return (
-    <div id="comparison-modal">
-      <table>
-        <tr>
-          <th>{examples.product.name}</th>
-          <th></th>
-          <th>{product2.name}</th>
-        </tr>
-        {features.map((feature) => (
-          <tr>
-            <td>{feature.value1 ? feature.value1 : ''}</td>
-            <td>{feature.feature}</td>
-            <td>{feature.value2 ? feature.value2 : ''}</td>
-          </tr>
-        ))}
-      </table>
-      <button onClick={() => setShowModal(false)}>Close</button>
-    </div>
+    <>
+      <div id="comparison-modal">
+        <div className="comparison-modal-content">
+          <header>Comparing</header>
+          <div className="comparison-table">
+            <div>{examples.product.name}</div>
+            <div></div>
+            <div>{product2.name}</div>
+            {features.map((feature) => (
+              <div className="comparison-table-row">
+                <div>{feature.value1 ? feature.value1 : ''}</div>
+                <div>{feature.feature}</div>
+                <div>{feature.value2 ? feature.value2 : ''}</div>
+              </div>
+            ))}
+          </div>
+          <button
+            className="comparison-modal-button"
+            onClick={() => setShowModal(false)}
+            >
+          &times;
+          </button>
+        </div>
+      </div>
+      <div className="comparison-modal-overlay">
+      </div>
+    </>
   )
 }
 
