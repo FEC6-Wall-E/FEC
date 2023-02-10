d6d06bded2f1129db5d9fa48414fa19db7b1a326
Daniel Greaves <dgreaves18@gmail.com>
2023-02-10 10:09:32 -0800

    Cleaned up modal, fixed dark theme

--- client/src/components/qa/AddQuestion.jsx
+++ client/src/components/qa/AddQuestion.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import api from './lib/qaRequests.js';
 
 function AddQuestion({
-  name, id, setQuestOpen, getQuestions,
+  name, id, setQuestOpen, getQuestions, theme,
 }) {
   const [questionInput, setQuestionInput] = useState('');
   const [questionName, setQuestionName] = useState('');
@@ -25,52 +25,50 @@ function AddQuestion({
         console.log(err);
       });
   };
-  const content = new Array(1).fill(
-    <div>
-      <h1 className="qa-add-title">Ask Your Question </h1>
-      <h2>About: {name}</h2>
-      <form onSubmit={handleAddQuestion}>
-        <h3>Your question <span style={{ color: 'red' }}>*</span></h3>
-        <textarea
-          className="qa-add-input"
-          rows="5"
-          cols="40"
-          maxLength="1000"
-          placeholder="Why did you like the product or not?"
-          onChange={(e) => { setQuestionInput(e.target.value); }}
-        />
-        <h3>What is your nickname? <span style={{ color: 'red' }}>*</span></h3>
-        <input
-          className="qa-add-name"
-          type="text"
-          maxLength="60"
-          placeholder="Example: jackson11!"
-          onChange={(e) => { setQuestionName(e.target.value); }}
-          required
-        />
-        <div className="qa-add-subtext">for privacy reasons, do not use your full name or email address</div>
-        <h3>Your email <span style={{ color: 'red' }}>*</span></h3>
-        <input
-          className="qa-add-email"
-          type="email"
-          maxLength="60"
-          placeholder="Example: jack@email.com"
-          onChange={(e) => { setQuestionEmail(e.target.value); }}
-          required
-        />
-        <div className="qa-add-subtext">for authentication reasons, you will not be emailed</div>
-        <button type="submit" className="qa-add-submit">Submit</button>
-      </form>
-    </div>,
-  );
 
   return (
     <div>
-      <div className="qa-modal-shadow" onClick={close} />
-      <div className="qa-modal">
-        <div className="qa-modal-content">
-          <span onClick={close} className="close-button">&times;</span>
-          {content}
+      <div className={`qa-modal-shadow ${theme}`} onClick={close} />
+      <div className={`qa-modal ${theme}`}>
+        <div className={`qa-modal-content ${theme}`}>
+          <span onClick={close} className={`close-button ${theme}`}>&times;</span>
+          <div>
+            <h1 className={`qa-add-title ${theme}`}>Ask Your Question </h1>
+            <h2>About: {name}</h2>
+            <form onSubmit={handleAddQuestion}>
+              <span>(<span style={{ color: 'red' }}>*</span>) Mandatory</span>
+              <h3>Your question <span style={{ color: 'red' }}>*</span></h3>
+              <textarea
+                className={`qa-add-input ${theme}`}
+                rows="5"
+                cols="40"
+                maxLength="1000"
+                placeholder="Why did you like the product or not?"
+                onChange={(e) => { setQuestionInput(e.target.value); }}
+              />
+              <h3>What is your nickname? <span style={{ color: 'red' }}>*</span></h3>
+              <input
+                className={`qa-add-name ${theme}`}
+                type="text"
+                maxLength="60"
+                placeholder="Example: jackson11!"
+                onChange={(e) => { setQuestionName(e.target.value); }}
+                required
+              />
+              <div className={`qa-add-subtext ${theme}`}>for privacy reasons, do not use your full name or email address</div>
+              <h3>Your email <span style={{ color: 'red' }}>*</span></h3>
+              <input
+                className={`qa-add-email ${theme}`}
+                type="email"
+                maxLength="60"
+                placeholder="Example: jack@email.com"
+                onChange={(e) => { setQuestionEmail(e.target.value); }}
+                required
+              />
+              <div className={`qa-add-subtext ${theme}`}>for authentication reasons, you will not be emailed</div>
+              <button type="submit" className="qa-add-submit">Submit</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
